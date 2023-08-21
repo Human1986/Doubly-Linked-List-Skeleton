3450a435491df3a62134eee9c4685c10205638d2
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2023-08-21 23:23:22 +0200

    Task done!

--- src/main/java/com/epam/rd/autocode/dllist/DoublyLinkedListImpl.java
+++ src/main/java/com/epam/rd/autocode/dllist/DoublyLinkedListImpl.java
@@ -2,66 +2,190 @@ package com.epam.rd.autocode.dllist;
 
 import java.util.Optional;
 
+class Node {
+
+    Object element;
+    Node next;
+    Node prev;
+
+    Node(Object obj, Node prev, Node next) {
+        this.element = obj;
+        this.next = next;
+        this.prev = prev;
+
+    }
+
+}
+
 public class DoublyLinkedListImpl implements DoublyLinkedList {
 
-	private Node head;
-	
-	private Node tail;
+    private Node head;
+    private Node tail;
 
-	private static class Node {
+    private int getSize() {
+        int size = 0;
+        Node current = head;
+        while (current != null) {
+            size++;
+            current = current.next;
+        }
+        return size;
+    }
 
-		Object element;
+    @Override
+    public boolean addFirst(Object element) {
+        Node newNode = new Node(element, null, null);
+        if (head == null) {
+            head = tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
 
-		Node next;
+        }
+        return true;
+    }
 
-		Node prev;
+    @Override
+    public boolean addLast(Object element) {
+        Node new_Node = new Node(element, null, null);
+        if (element == null) return false;
 
-		Node(Object obj, Node prev, Node next) {
-			this.element = obj;
-			this.next = next;
-			this.prev = prev;
-		}
+        if (head == null) {
+            head = new_Node;
 
-	}
-	
-	@Override
-	public boolean addFirst(Object element) {
-		return false;
-	}
+            return true;
+        }
+        Node current = head;
 
-	@Override
-	public boolean addLast(Object element) {
-		return false;
-	}
+        while (current.next != null)
+            current = current.next;
 
-	@Override
-	public void delete(int index) {
-		
-	}
-	
-	@Override
-	public Optional<Object> remove(Object element) {
-		return null;
-	}
+        current.next = new_Node;
 
-	@Override
-	public boolean set(int index, Object element) throws IndexOutOfBoundsException {
-		return false;
-	}
+        new_Node.prev = current;
 
-	@Override
-	public int size() {
-		return 0;
-	}
+        return true;
+    }
 
-	@Override
-	public Object[] toArray() {
-		return null;
-	}
+    @Override
+    public void delete(int index) {
+        if (index == 0) {
+            removeFirst();
+        } else if (index == getSize()) {
+            removeLast();
+        } else {
+            remove(index);
 
-	@Override
-	public String toString() {
-		return null;
-	}
-	
+        }
+    }
+
+    private void remove(int index) {
+        Node previousNode = findNodeByIndex(index - 1);
+        previousNode.next = previousNode.next.next;
+    }
+
+
+    private Node findNodeByIndex(int index) {
+        Node current = head;
+        for (int i = 0; i < index; i++) {
+            if (current.next != null) {
+                current = current.next;
+            }
+        }
+        return current;
+    }
+
+    private void removeFirst() {
+        head = head.next;
+
+    }
+
+    private void removeLast() {
+        if (tail != null) {
+            tail = tail.prev;
+            tail.next = null;
+        }
+
+    }
+
+    private boolean isEmpty() {
+        return head == null;
+    }
+
+    @Override
+    public Optional<Object> remove(Object element) {
+        if (element == null) {
+            throw new NullPointerException();
+        }
+        if (! isEmpty()) {
+            Node current = head;
+            while (current != null) {
+                if (current.element == element) {
+                    current.prev = current.next;
+
+                    return Optional.of(current.element);
+                }
+                current = current.next;
+            }
+        }
+        return Optional.empty();
+
+    }
+
+    @Override
+    public boolean set(int index, Object element) throws IndexOutOfBoundsException {
+        if (index < 0 || index >= getSize()) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        if (element == null) return false;
+
+        Node newNode = new Node(element, null, null);
+
+        if (index == 0) {
+            newNode.next = head;
+            head = newNode;
+        } else {
+            Node prevNode = findNodeByIndex(index - 1);
+            newNode.next = prevNode.next.next;
+            prevNode.next = newNode;
+
+        }
+        return true;
+    }
+
+
+    @Override
+    public int size() {
+        return getSize();
+    }
+
+    @Override
+    public Object[] toArray() {
+        Object[] result = new Object[getSize()];
+        int i = 0;
+        Node current = head;
+        while (i < getSize()) {
+            if (current != null) {
+                result[i++] = current.element;
+                current = current.next;
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        Node current = head;
+        while (current != null) {
+            builder.append(current.element);
+            current = current.next;
+            if (current != null) {
+                builder.append(" ");
+            }
+        }
+        return builder.toString();
+    }
+
 }
